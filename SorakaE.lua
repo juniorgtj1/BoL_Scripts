f37dfbb065522c6624f6a8400fea6c56fe3e5789
pqmailer <pqmailer@gmail.com>
2013-07-13 22:00:03 +0200

    BttB

--- SorakaE.lua
+++ SorakaE.lua
@@ -1,31 +1,22 @@
-
 --[[ Simple script that casts E at the nearest enemy if you press Q on Soraka ]]--
 
---[[ Variables ]]--
-local NearestEnemy = nil -- our nearest champ
+local NearestEnemy = nil
 
---[[ Core ]]--
-function OnLoad()
-    print(">>Soraka auto E loaded<<") -- say hello
+function OnWndMsg(msg, wParam) -- Captures the pressed keys
+    if msg == KEY_DOWN and wParam == 0x51 then CastE() end -- 0x51 = Q
 end
 
--- Captures the pressed keys
-function OnWndMsg(msg, wParam)
-    if msg == KEY_DOWN and wParam == 0x51 then CastE() end -- 0x51 == Q key
-end
-
--- Casts E on the nearest enemy champ
 function CastE()
-    for i=1, heroManager.iCount do -- loop through the enemys
+    for i=1, heroManager.iCount do -- first we get the nearest champ
         local Enemy = heroManager:GetHero(i)
         if ValidTarget(NearestEnemy) and ValidTarget(Enemy) then
             if GetDistance(Enemy) < GetDistance(NearestEnemy) then
-                NearestEnemy = Enemy -- and find the nearest champ
+                NearestEnemy = Enemy
             end
         else
             NearestEnemy = Enemy
         end
     end
 
-    if myHero:GetDistance(NearestEnemy) <= 725 then CastSpell(_E, NearestEnemy) end -- finally cast our spell
+    if myHero:GetDistance(NearestEnemy) <= 725 and wait == 1 then CastSpell(_E, NearestEnemy) end
 end
