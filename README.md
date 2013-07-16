77620c8bc26d8c2ba0dfb9c589ba2d9d5e39ae7f
pqmailer <pqmailer@gmail.com>
2013-07-17 01:00:06 +0200

    Added Garen Plugin

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 SidasAutoCarryPlugin - Ryze.lua
 -------------------------------
 An addition to Sidas Auto Carry. Put it in *BoL/Scripts/Common*. It loads automatically in Auto Carry.
+SidasAutoCarryPlugin - Garen.lua
+--------------------------------
+A simple Auto Carry Plugin that uses Ulti/Barrier/Ignite. Put it in *BoL/Scripts/Common*. It loads automatically in Auto Carry.
 Ryze.lua
 --------
 Not fully working at the moment (requires iSAC). Put it in *BoL/Scripts*. You should use the Auto Carry Plugin.
---
+++ SidasAutoCarryPlugin - Garen.lua
@@ -0,0 +1,53 @@
+--[[ Simple Garen Plugin ]]--
+
+local SpellRangeR = 400
+local RReady, IGNITEReady, BARRIERReady = nil, nil, nil
+
+function PluginOnLoad()
+	AutoCarry.PluginMenu:addParam("aUlt", "Use Ulti", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("aIGN", "Use Ignite", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("aB", "Use Barrier", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("lr", "Barrier Life Ratio", SCRIPT_PARAM_SLICE, 0.15, 0, 1, 2)
+
+	IGNITESlot = ((myHero:GetSpellData(SUMMONER_1).name:find("SummonerDot") and SUMMONER_1) or (myHero:GetSpellData(SUMMONER_2).name:find("SummonerDot") and SUMMONER_2) or nil)
+	BARRIERSlot = ((myHero:GetSpellData(SUMMONER_1).name:find("SummonerBarrier") and SUMMONER_1) or (myHero:GetSpellData(SUMMONER_2).name:find("SummonerBarrier") and SUMMONER_2) or nil)
+end
+
+function PluginOnTick()
+	CooldownHandler()
+	if AutoCarry.PluginMenu.aUlt then CastR() end
+	if AutoCarry.PluginMenu.aB then AutoBarrier() end
+	if AutoCarry.PluginMenu.aIGN then AutoIgnite() end
+end
+
+function CastR()
+	if not RReady then return true end
+
+	for _, enemy in pairs(AutoCarry.EnemyTable) do
+		if ValidTarget(enemy, SpellRangeR) and GetDmg(_R, enemy, myHero) >= enemy.health then CastSpell(_R, enemy) end
+	end
+end
+
+function AutoBarrier()
+	if myHero.health/myHero.maxHealth < AutoCarry.PluginMenu.lr and BARRIERReady then CastSpell(BARRIERSlot) end
+end
+
+function AutoIgnite()
+	if not IGNITEReady then return true end
+
+	local IGNITEDamage = 0
+	for _, enemy in pairs(AutoCarry.EnemyTable) do
+		if ValidTarget(enemy, 600) then
+			IGNITEDamage = 50 + 20 * myHero.level
+			if IGNITEDamage >= enemy.health then
+				CastSpell(IGNITESlot, enemy)
+			end
+		end
+	end
+end
+
+function CooldownHandler()
+	RREADY = (myHero:CanUseSpell(_R) == READY)
+	IGNITEReady = (IGNITESlot ~= nil and myHero:CanUseSpell(IGNITESlot) == READY)
+	BARRIERReady = (BARRIERSlot ~= nil and myHero:CanUseSpell(BARRIERSlot) == READY)
+end
