e41080c80a79e9e5629a562abb7f0905521d02ef
pqmailer <pqmailer@gmail.com>
2013-07-19 21:30:43 +0200

    Blaa

--- Not my Stuff/stuff.lua
+++
@@ -1,6 +0,0 @@
-Collision:__init(sRange, projSpeed, sDelay, sWidth) --> sRange : Range of spell --> projSpeed : speed of spell, sDelay : Delay in s --> sWidth : Spell width of the spell
-Collision:GetMinionCollision(pStart, pEnd)   -> pStart :  startposition of the spell --> pEnd : End position of the spell(Prediction)
-Collision:GetHeroCollision(pStart, pEnd, mode) --> Mode : HERO_ALL,  HERO_ENEMY, HERO_ALLY
-Collision:GetCollision(pStart, pEnd)
-Collision:DrawCollision(pStart, pEnd)
-
