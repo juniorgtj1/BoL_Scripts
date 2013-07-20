14c774708ccc272918060525f04809931bd7d206
pqmailer <pqmailer@gmail.com>
2013-07-20 13:34:30 +0200

    Initial Commit
    ADD: Enemy range

---
+++ EnemyRange.lua
@@ -0,0 +1,28 @@
+--[[ Shows the enemy range ]]--
+
+local champs = {
+	{name = "Ashe", range = 600},
+	{name = "Caitlyn", range = 650},
+	{name = "Corki", range = 550},
+	{name = "Draven", range = 550},
+	{name = "Ezrael", range = 550},
+	{name = "Graves", range = 525},
+	{name = "KogMaw", range = 500},
+	{name = "MissFortune", range = 550},
+	{name = "Sivir", range = 500},
+	{name = "Tristana", range = 550},
+	{name = "Twitch", range = 550},
+	{name = "Varus", range = 575},
+	{name = "Vayne", range = 550}
+}
+
+function OnDraw()
+	for i=1, heroManager.iCount do
+		local Unit = heroManager:GetHero(i)
+		for _,champ in pairs(champs) do
+			if champ.name == Unit.charName then
+				DrawCircle(Unit.x, Unit.y, Unit.z, champ.range, 0xFFFFFF00)
+			end
+		end
+	end
+end
