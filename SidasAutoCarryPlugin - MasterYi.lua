134f0aa2d0553f0054fe204de41ffee667f38c14
pqmailer <pqmailer@gmail.com>
2013-08-10 15:53:18 +0200

    Major Update
    Tested, Fixed, Added Jump

--- SidasAutoCarryPlugin - MasterYi.lua
+++ SidasAutoCarryPlugin - MasterYi.lua
@@ -1,16 +1,20 @@
 local QReady, EReady, RReady = nil, nil, nil
-local RangeQ, RangeMeele = 600, 150
+local RangeQ, RangeMeele, RangeJump = 600, 150, 150
 
 function PluginOnLoad()
 	AutoCarry.PluginMenu:addParam("autoR", "Auto R in Combo", SCRIPT_PARAM_ONOFF, true)
 	AutoCarry.PluginMenu:addParam("ks", "KS with Q", SCRIPT_PARAM_ONOFF, true)
 	AutoCarry.PluginMenu:addParam("dqR", "Draw Q Range", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("qHarass", "Minion Q Harass", SCRIPT_PARAM_ONOFF, true)
+
+	AutoCarry.SkillsCrosshair.range = RangeQ
 end
 
 function PluginOnTick()
 	CDHandler()
 	if AutoCarry.PluginMenu.ks then KS() end
 	if AutoCarry.MainMenu.AutoCarry then Combo() end
+	if AutoCarry.MainMenu.MixedMode and AutoCarry.PluginMenu.qHarass then MinionQHero() end
 end
 
 function PluginOnDraw()
@@ -43,4 +47,36 @@ function Combo()
 		if EReady and ((QReady and Distance <= RangeQ) or (Distance <= RangeMeele)) then CastSpell(_E) end
 		if QReady and Distance <= RangeQ then CastSpell(_Q, Target) end
 	end
+end
+
+function MinionQHero()
+    local Minions = {}
+    local NearestMinion = nil
+    local MinionCount = 0
+ 
+        for index, minion in pairs(AutoCarry.EnemyMinions().objects) do
+                if ValidTarget(minion) then
+                        if GetDistance(minion) <= 600 then
+                for _, enemy in pairs(AutoCarry.EnemyTable) do
+                    if GetDistance(minion, enemy) <= RangeJump then
+                        table.insert(Minions, minion)
+                        MinionCount = MinionCount + 1
+                    end
+                end
+            end
+                end
+        end
+ 
+    if MinionCount == 0 then return end
+    for _, jumpTarget in pairs(Minions) do
+        if NearestMinion and NearestMinion.valid and jumpTarget and jumpTarget.valid then
+            if GetDistance(jumpTarget) < GetDistance(NearestMinion) then
+                NearestMinion = jumpTarget
+            end
+        else
+            NearestMinion = jumpTarget
+        end
+    end
+ 
+    if ValidTarget(NearestMinion) and MinionCount <= 3 and QReady then CastSpell(_Q, NearestMinion) end
 end
